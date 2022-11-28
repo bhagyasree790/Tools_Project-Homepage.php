e979554f8f365f9c0dd71e313a0007fd6085d50b
bhagyasree790 <71458687+bhagyasree790@users.noreply.github.com>
2022-11-28 23:55:12 +0600

    Create homepage.php

---
+++ homepage.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>HOME PAGE</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style_about.css">
+    <link rel="stylesheet" type="text/css" href="service.css">
+
+</head>
+<body>
+
+    <div class="main" id="homee">
+        <div class="navbar">
+            <div class="icon">
+                <h2 class="logo">PraDaxa</h2>
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li><a href="Demo.php">HOME</a></li>
+                    <li><a href="#aboutt">ABOUT</a></li>
+                    <li><a href="#servicee">SERVICE</a></li>
+                    <li><a href="Doner_list.php">DONER LIST</a></li>
+                    <li><a href="reg_form.php">BE A DONER</a></li>
+                </ul>
+            </div>
+        </div> 
+        <div class="content">
+            <h1>Health & <br><span>Humanity</span></h1>
+            <p class="par">A single drop of blood can make a huge difference.
+                <br>Instead of giving your blood to mosquitoes, give it to someone who needs it.<br>Be a saviour just by donating your blood. A blood donor is equal to a lifesaver.<br></p>
+
+                
+                <div class="form">
+                    <h2>Login Here</h2>
+                    <input type="email" name="email" placeholder="Enter Email Here">
+                    <input type="password" name="" placeholder="Enter Password Here">
+                    <button class="btnn"><a href="#">Login</a></button>
+
+                    <p class="link">Don't have an account<br>
+                    <a href="Registration.html">Sign up </a> here</a></p>
+                    
+                </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+        <section class="about" id="aboutt">
+            <div class="main_about">
+                
+                <div class="about-text">
+                    <div class="about-text-h2"><span style="--i:1">A</span>
+                        <span style="--i:2">B</span>
+                        <span style="--i:3">O</span>
+                        <span style="--i:4">U</span>
+                        <span style="--i:5">T</span>
+                        <span style="--i:6"> </span>
+                        <span style="--i:7">U</span>
+                        <span style="--i:8">S</span></div>
+                    <h5 class="about-text h5">Health <span>& Humanity</span></h5>
+                    <p>Our main goal is to bring the blood donors of Bangladesh within your reach through this website. Even if you are willing to donate blood, you can easily reach blood recipients through this website. </p>
+                    
+                    <button class="button_about"><a href="#"><b>Log in</b></a></button>
+                    
+                </div>
+                <img src="aboutimg.jpg" class = "about img">
+            </div>
+        </section> 
+
+        <section class="service" id="servicee">
+            <div class="title">
+                <span style="--i:1">O</span>
+                   <span style="--i:2">U</span>
+                   <span style="--i:3">R</span>
+                   <span style="--i:4"> </span>
+                   <span style="--i:5">S</span>
+                   <span style="--i:6">E</span>
+                   <span style="--i:7">R</span>
+                   <span style="--i:8">V</span>
+                   <span style="--i:9">I</span>
+                   <span style="--i:10">C</span>
+                <span style="--i:11">E</span>
+            </div>
+    
+            <div class="box">
+                <div class="card">
+                    <i class="fas fa-bars"></i>
+                    <h5>Doner's Info</h5>
+                    <div class="pra">
+                        <p>You can get blood doners here.</p>
+    
+                        <p style="text-align: center;">
+                            <a class="button" href="doner_list.html">Read More</a>
+                        </p>
+                    </div>
+                </div>
+    
+                <div class="card">
+                    <i class="far fa-bell"></i>
+                    <h5>Registration</h5>
+                    <div class="pra">
+                        <p>You can register your information here so that people can find you easily.</p>
+    
+                        <p style="text-align: center;">
+                            <a class="button" href="Registration.html">Read More</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <script src="server.js"></script>
+</body>
+</html>
